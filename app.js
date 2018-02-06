9afde7a3762e2f85760e0738d28da459ce31b0fc
Carlos A. Dendarys <carlos@thirdlove.com>
2018-02-06 17:10:36 -0300

    I added circleci configurations.

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ var bodyParser = require('body-parser');
 // New Code
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/nodetest1');
+// var db = monk('localhost:27017/nodetest1');
+
+var db = monk(process.env.MONGO_DB_URL);
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
---
+++ setup-env.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+export MONGO_DB_URL=cadmongo:test1234@ds225608.mlab.com:25608/nodetest1
